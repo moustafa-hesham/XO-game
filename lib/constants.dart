361e3861be39ea1a284ca0620e2df9f7a6326093
moustafa-hesham <113013570+moustafa-hesham@users.noreply.github.com>
2023-09-09 02:50:26 +0300

    added logo in home page

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 Color kPrimaryColor = const Color(0xff323D5B);
 Color kSecondColor = const Color(0xff0A1735);
 
-Color kXColor = const Color.fromARGB(255, 16, 208, 172);
+Color kXColor = Color(0xFF10D0AC);
 
-Color kOColor = const Color.fromARGB(255, 62, 168, 255);
+Color kOColor = Color(0xFF3EA8FF);
